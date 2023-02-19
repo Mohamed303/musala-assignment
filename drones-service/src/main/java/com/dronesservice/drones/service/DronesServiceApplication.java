b86da66fca6e689312f8b7f18709dc8b5fe409fc
Mohamed Sobhy <mosobhy@Mohameds-MacBook-Pro.local>
2023-02-19 12:19:25 +0200

    [add service] upload drones service

---
+++ drones-service/src/main/java/com/dronesservice/drones/service/DronesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dronesservice.drones.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronesServiceApplication.class, args);
+	}
+
+}
