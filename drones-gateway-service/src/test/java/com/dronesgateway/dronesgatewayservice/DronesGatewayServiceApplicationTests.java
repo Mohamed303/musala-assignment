ac5a5a2fea85562f10d4cdc10f9b9a5096387101
Mohamed Sobhy <mosobhy@Mohameds-MacBook-Pro.local>
2023-02-27 21:39:19 +0200

    [setup] setup gatway service, [configration] configure yml files for monitoring service and drones service with server ports and database connection

---
+++ drones-gateway-service/src/test/java/com/dronesgateway/dronesgatewayservice/DronesGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronesgateway.dronesgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronesGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
